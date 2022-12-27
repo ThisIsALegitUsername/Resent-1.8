f3b61c27e49d1b9fad7876320f8efc3934e75e77
ThisIsALegitUsername <andrewsouchien@gmail.com>
2022-12-27 22:23:06 +0000

    Try to fix crash part 1

--- src/main/java/net/minecraft/client/settings/GameSettings.java
+++ src/main/java/net/minecraft/client/settings/GameSettings.java
@@ -205,7 +205,7 @@ public class GameSettings {
 				this.keyBindSneak, this.keyBindSprint, this.keyBindDrop, this.keyBindInventory, this.keyBindChat,
 				this.keyBindPlayerList, this.keyBindPickBlock, this.keyBindCommand, this.keyBindScreenshot,
 				this.keyBindTogglePerspective, this.keyBindSmoothCamera, this.keyBindZoomCamera, this.keyBindFunction,
-				this.keyBindClose }, this.keyBindsHotbar);
+				this.keyBindClose, this.keyBindClickGui }, this.keyBindsHotbar);
 		this.difficulty = EnumDifficulty.NORMAL;
 		this.lastServer = "";
 		this.fovSetting = 70.0F;
