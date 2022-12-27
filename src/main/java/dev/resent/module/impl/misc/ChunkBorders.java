c99577a013546899f71accce293ac615a7635439
ThisIsALegitUsername <andrewsouchien@gmail.com>
2022-12-27 21:44:26 +0000

    Remove chunkborders

--- src/main/java/dev/resent/module/impl/misc/ChunkBorders.java
+++ src/main/java/dev/resent/module/impl/misc/ChunkBorders.java
@@ -1,40 +1,40 @@
-package dev.resent.module.impl.misc;
+// package dev.resent.module.impl.misc;
 
-import dev.resent.Resent;
-import dev.resent.event.impl.RenderWorldEvent;
-import dev.resent.module.base.Category;
-import dev.resent.module.base.Mod;
-GlStateManager.popMatrix();
-import net.minecraft.src.AxisAlignedBB;
-import net.minecraft.src.RenderGlobal;
-import net.minecraft.src.RenderManager;
+// import dev.resent.Resent;
+// import dev.resent.event.impl.RenderWorldEvent;
+// import dev.resent.module.base.Category;
+// import dev.resent.module.base.Mod;
+// import net.lax1dude.eaglercraft.v1_8.opengl.GlStateManager;
+// import net.minecraft.client.renderer.RenderGlobal;
+// import net.minecraft.client.renderer.entity.RenderManager;
+// import net.minecraft.util.AxisAlignedBB;
 
-public class ChunkBorders extends Mod{
-    public ChunkBorders() {
-        super("ChunkBorders", Category.MISC);
-        Resent.INSTANCE.events().subscribe(RenderWorldEvent.class, event -> {
-            if (this.isEnabled() && mc.theWorld != null) {
+// public class ChunkBorders extends Mod{
+//     public ChunkBorders() {
+//         super("ChunkBorders", Category.MISC);
+//         Resent.INSTANCE.events().subscribe(RenderWorldEvent.class, event -> {
+//             if (this.isEnabled() && mc.theWorld != null) {
             
-            final int chunkX = mc.thePlayer.chunkCoordX * 16;
-            final int chunkZ = mc.thePlayer.chunkCoordZ * 16;
+//             final int chunkX = mc.thePlayer.chunkCoordX * 16;
+//             final int chunkZ = mc.thePlayer.chunkCoordZ * 16;
 
-            final AxisAlignedBB chunkBB = AxisAlignedBB.getBoundingBox(
-                    chunkX, 0.0, chunkZ,
-                    chunkX + 16.0, 255.0, chunkZ + 16.0);
+//             final AxisAlignedBB chunkBB = AxisAlignedBB.getBoundingBox(
+//                     chunkX, 0.0, chunkZ,
+//                     chunkX + 16.0, 255.0, chunkZ + 16.0);
 
-            GlStateManager.pushMatrix();
-            EaglerAdapter.glColor3f(1.0f, 1.0f, 0.0f);
-            EaglerAdapter.glDisable(EaglerAdapter.GL_TEXTURE_2D);
-            EaglerAdapter.glDisable(EaglerAdapter.GL_DEPTH_TEST);
-            EaglerAdapter.glTranslatef((float)-RenderManager.renderPosX, (float)-RenderManager.renderPosY, (float)-RenderManager.renderPosZ);
-            EaglerAdapter.glLineWidth(1f);
+//             GlStateManager.pushMatrix();
+//             GlStateManager.color(1.0f, 1.0f, 0.0f);
+//             GlStateManager.disableTexture2D();
+//             GlStateManager.disableDepth();
+//             GlStateManager.translate((float)-RenderManager.renderPosX, (float)-RenderManager.renderPosY, (float)-RenderManager.renderPosZ);
+//             GlStateManager.line(1f);
 
-            RenderGlobal.drawOutlinedBoundingBox(chunkBB);
-            EaglerAdapter.glEnable(EaglerAdapter.GL_DEPTH_TEST);
-            EaglerAdapter.glEnable(EaglerAdapter.GL_TEXTURE_2D);
-            GlStateManager.popMatrix();
-            }
-        });
-    }
+//             RenderGlobal.drawOutlinedBoundingBox(chunkBB);
+//             GlStateManager.enableDepth();
+//             GlStateManager.enableTexture2D();
+//             GlStateManager.popMatrix();
+//             }
+//         });
+//     }
     
-}
+// }
--- src/main/java/net/minecraft/client/renderer/entity/RenderManager.java
+++ src/main/java/net/minecraft/client/renderer/entity/RenderManager.java
@@ -126,9 +126,9 @@ public class RenderManager {
 	private Map<String, RenderPlayer> skinMap = Maps.newHashMap();
 	private RenderPlayer playerRenderer;
 	private FontRenderer textRenderer;
-	private double renderPosX;
-	private double renderPosY;
-	private double renderPosZ;
+	public double renderPosX;
+	public double renderPosY;
+	public double renderPosZ;
 	public TextureManager renderEngine;
 	public World worldObj;
 	public Entity livingPlayer;
