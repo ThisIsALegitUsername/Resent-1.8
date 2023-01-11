851bc61255846d3c1d670e72bece747dc0893aba
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-01-11 00:41:46 +0000

    cancer

--- src/main/java/dev/resent/module/impl/hud/PotionHUD.java
+++ src/main/java/dev/resent/module/impl/hud/PotionHUD.java
@@ -37,48 +37,37 @@ public class PotionHUD extends RenderModule{
         int i2 = 16;
         Collection<PotionEffect> collection = mc.thePlayer.getActivePotionEffects();
         if (!collection.isEmpty()) {
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            GlStateManager.disableLighting();
             int l = 33;
             if (collection.size() > 5)
                 l = 132 / (collection.size() - 1); 
             for (PotionEffect potioneffect : mc.thePlayer.getActivePotionEffects()) {
                 Potion potion = Potion.potionTypes[potioneffect.getPotionID()];
                 GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+			GlStateManager.disableLighting();
+			GlStateManager.enableAlpha();
                 if (potion.hasStatusIcon()) {
-                    GuiIngame guiIngame = new GuiIngame(mc);
-                    GlStateManager.enableAlpha();
-                    //GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-                    GlStateManager.enableBlend();
-                    GlStateManager.tryBlendFuncSeparate(775, 769, 1, 0);
                     
+                    GuiIngame guiIngame = new GuiIngame(mc);
                     Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("textures/gui/container/inventory.png"));
                     int i3 = potion.getStatusIconIndex();
-                    
-                    guiIngame.drawTexturedModalRect(getX() + 21 - 14, getY() + i2 - 14, 0 + i3 % 8 * 18, 198 + i3 / 8 * 18, 18, 18);
+                    GlStateManager.tryBlendFuncSeparate(1, 1, 1, 0);
+                    guiIngame.drawTexturedModalRect(getX() + 21 - 20, getY() + i2 - 14, 0 + i3 % 8 * 18, 198 + i3 / 8 * 18, 18, 18);
                 } 
+
                 String s1 = I18n.format(potion.getName(), new Object[0]);
                 if (potioneffect.getAmplifier() == 1) {
-                    s1 = "I";
+                    s1 = String.valueOf(String.valueOf(s1)) + " " + I18n.format("enchantment.level.2", new Object[0]);
                 } else if (potioneffect.getAmplifier() == 2) {
-                    s1 = "II";
+                    s1 = String.valueOf(String.valueOf(s1)) + " " + I18n.format("enchantment.level.3", new Object[0]);
                 } else if (potioneffect.getAmplifier() == 3) {
-                    s1 = "III";
-                } else if (potioneffect.getAmplifier() == 4) {
-                    s1 = "IV";
-                } else if (potioneffect.getAmplifier() == 5) {
-                    s1 = "V";
-                } else {
-                    s1 = ("" + potioneffect.getAmplifier());
-                }
-
-                mc.fontRendererObj.drawString(s1, (getX() + 21), (getY() + i2 - 14), 16777215, true);
+                    s1 = String.valueOf(String.valueOf(s1)) + " " + I18n.format("enchantment.level.4", new Object[0]);
+                } 
+                mc.fontRendererObj.drawString(s1, (getX() + 21), (getY() + i2 - 14), -1, true);
                 String s2 = Potion.getDurationString(potioneffect);
-                mc.fontRendererObj.drawString(s2, (getX() + 21), (getY() + i2 + 10 - 14), 8355711, true);
+                mc.fontRendererObj.drawString(s2, (getX() + 21), (getY() + i2 + 10 - 14), -1, true);
                 i2 += l;
             } 
         } 
-        super.draw();
     }
 
     
