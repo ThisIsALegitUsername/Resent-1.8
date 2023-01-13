f5c3ccee43d6aaad4f3cfa45a67495c1769f7956
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-01-13 17:46:47 +0000

    offline download

--- offline.sh
+++ offline.sh
@@ -2,8 +2,9 @@
 cd javascript
 rm EaglercraftX_1.8_Offline_en_US.html
 rm EaglercraftX_1.8_Offline_International.html
+rm Resent_EaglercraftX_1.8_US.html
+rm Resent_EaglercraftX_1.8_International.html
 cd ../
-chmod +x ./MakeOfflineDownload.sh
 ./MakeOfflineDownload.sh
 cd javascript
 mv EaglercraftX_1.8_Offline_en_US.html Resent_EaglercraftX_1.8_US.html
