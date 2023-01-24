aafb54d8e9c32a734a4789ca1495cfb775e78e74
UnknownUser1789 <ihavepotatoesinmyfridge@gmail.com>
2023-01-24 02:35:34 +0000

    Rename crystal optimizer

--- src/main/java/dev/resent/module/impl/misc/CrystalOptimizer.java
+++ src/main/java/dev/resent/module/impl/misc/CrystalOptimizer.java
@@ -5,7 +5,7 @@ import dev.resent.module.base.Mod;
 
 public class CrystalOptimizer extends Mod{
     public CrystalOptimizer(){
-        super("Crystal Fast", Category.MISC);
+        super("Fast Crystal", Category.MISC);
     }
     
 }
