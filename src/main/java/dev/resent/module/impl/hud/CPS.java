9fa5efbebd73292a3fee6be86a263611dbd8d8b3
UnknownUser1789 <ihavepotatoesinmyfridge@gmail.com>
2023-01-25 18:48:57 +0000

    some bug fixes

--- src/main/java/dev/resent/module/impl/hud/CPS.java
+++ src/main/java/dev/resent/module/impl/hud/CPS.java
@@ -12,32 +12,26 @@ import net.lax1dude.eaglercraft.v1_8.Mouse;
 public class CPS extends RenderModule {
 
     public CPS() {
-        super("CPS", Category.HUD, 4, 84, true);
+        super("CPS", Category.HUD, 50, 4, true);
         addSetting(tshadow);
     }
 
-    private final List<Long> clicks = new ArrayList<>();
+    private List<Long> clicks = new ArrayList<>();
     private boolean wasPressed;
     private long lastPressed;
 
     public BooleanSetting tshadow = new BooleanSetting("Text shadow", "", true);
-
-    public int getWidth() {
-        return mc.fontRendererObj.getStringWidth("[00 CPS]") + 4;
-    }
-
-    public int getHeight() {
-        return mc.fontRendererObj.FONT_HEIGHT + 4;
-    }
+    public int getWidth() { return mc.fontRendererObj.getStringWidth("[CPS: 00]") + 4; }
+    public int getHeight() { return mc.fontRendererObj.FONT_HEIGHT+4; }
 
     @Override
     public void draw() {
         final boolean pressed = Mouse.isButtonDown(0) || Mouse.isButtonDown(1);
 
-        if (pressed != wasPressed) {
+        if(pressed != wasPressed){
             lastPressed = System.currentTimeMillis();
             wasPressed = pressed;
-            if (pressed) {
+            if(pressed){
                 this.clicks.add(lastPressed);
             }
         }
@@ -45,6 +39,7 @@ public class CPS extends RenderModule {
         final long time = System.currentTimeMillis();
         FuncUtils.removeIf(clicks, aLong -> aLong + 1000 < time);
 
-        mc.fontRendererObj.drawString("[" + clicks.size() + " CPS]", this.x + 2, this.y + 2, -1, tshadow.getValue());
+        mc.fontRendererObj.drawString("CPS: " + clicks.size(), this.x+2, this.y+2, -1, tshadow.getValue());
     }
+
 }
