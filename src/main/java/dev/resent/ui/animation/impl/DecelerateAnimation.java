bd8c41de183651f7b1e79f25d6016b75b5f813c3
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-01 04:09:29 +0000

    Refactor packages

---
+++ src/main/java/dev/resent/ui/animation/impl/DecelerateAnimation.java
@@ -0,0 +1,20 @@
+package dev.resent.ui.animation.impl;
+
+import dev.resent.ui.animation.Animation;
+import dev.resent.ui.animation.Direction;
+
+public class DecelerateAnimation extends Animation {
+
+    public DecelerateAnimation(int ms, double endPoint) {
+        super(ms, endPoint);
+    }
+
+    public DecelerateAnimation(int ms, double endPoint, Direction direction) {
+        super(ms, endPoint, direction);
+    }
+
+    protected double getEquation(double x) {
+        double x1 = x / duration;
+        return 1 - ((x1 - 1) * (x1 - 1));
+    }
+}
---
+++ src/main/java/dev/resent/ui/animation/impl/EaseInOutQuad.java
@@ -0,0 +1,21 @@
+package dev.resent.ui.animation.impl;
+
+import dev.resent.ui.animation.Animation;
+import dev.resent.ui.animation.Direction;
+
+public class EaseInOutQuad extends Animation {
+
+    public EaseInOutQuad(int ms, double endPoint) {
+        super(ms, endPoint);
+    }
+
+    public EaseInOutQuad(int ms, double endPoint, Direction direction) {
+        super(ms, endPoint, direction);
+    }
+
+    protected double getEquation(double x1) {
+        double x = x1 / duration;
+        return x < 0.5 ? 2 * Math.pow(x, 2) : 1 - Math.pow(-2 * x + 2, 2) / 2;
+    }
+
+}
