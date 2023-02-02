b1739d7d880e5b26e7031e83ffa12f654515e1c4
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-02 16:28:01 +0000

    Port reachdisplay

--- src/main/java/dev/resent/module/impl/hud/ReachDisplay.java
+++ src/main/java/dev/resent/module/impl/hud/ReachDisplay.java
@@ -2,12 +2,14 @@ package dev.resent.module.impl.hud;
 
 import java.text.DecimalFormat;
 
+import dev.resent.annotation.RenderModule;
 import dev.resent.module.Theme;
 import dev.resent.module.base.Category;
 import dev.resent.module.base.RenderMod;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Vec3;
 
+@RenderModule(name = "ReachDisplay", category = Category.HUD, x = 4, y = 86)
 public class ReachDisplay extends RenderMod {
 
     public static final DecimalFormat df2 = new DecimalFormat("0.00");
