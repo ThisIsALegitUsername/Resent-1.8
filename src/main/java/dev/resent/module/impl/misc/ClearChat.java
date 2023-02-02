33fc28b3b6de82914b63288a793ab0e62c1673d3
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-02 16:33:42 +0000

    Port autorespawn

--- src/main/java/dev/resent/module/impl/misc/ClearChat.java
+++ src/main/java/dev/resent/module/impl/misc/ClearChat.java
@@ -1,8 +1,10 @@
 package dev.resent.module.impl.misc;
 
+import dev.resent.annotation.Module;
 import dev.resent.module.base.Category;
 import dev.resent.module.base.Mod;
 
+@Module
 public class ClearChat extends Mod {
 
     public ClearChat() {
