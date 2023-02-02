a9b1e1f54bc6391b6a4648cf5ceb4c31252c5a21
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-02 19:53:34 +0000

    a

--- src/main/java/dev/resent/module/impl/misc/NoHurtCam.java
+++ src/main/java/dev/resent/module/impl/misc/NoHurtCam.java
@@ -3,9 +3,5 @@ package dev.resent.module.impl.misc;
 import dev.resent.module.base.Category;
 import dev.resent.module.base.Mod;
 
-public class NoHurtCam extends Mod {
-
-    public NoHurtCam() {
-        super("NoHurtCam", Category.MISC);
-    }
-}
+@Module(name = "NoHurtCam", category = Category.MISC)
+public class NoHurtCam extends Mod { }
