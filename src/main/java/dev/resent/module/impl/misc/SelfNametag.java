766bf8ee9c09359bcc66fd0a9ea36df91ca530c8
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-02 19:57:15 +0000

    a

--- src/main/java/dev/resent/module/impl/misc/SelfNametag.java
+++ src/main/java/dev/resent/module/impl/misc/SelfNametag.java
@@ -1,8 +1,10 @@
 package dev.resent.module.impl.misc;
 
+import dev.resent.annotation.Module;
 import dev.resent.module.base.Category;
 import dev.resent.module.base.Mod;
 
+@Module(name = "Self Nametag", category = Category.MISC)
 public class SelfNametag extends Mod {
 
     public SelfNametag() {
