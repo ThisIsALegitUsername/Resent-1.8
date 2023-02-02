1022a6b561edf0a3ce08019e7c1c83435be618b9
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-02 16:39:23 +0000

    port dynfov

--- src/main/java/dev/resent/module/impl/misc/DynamicFOV.java
+++ src/main/java/dev/resent/module/impl/misc/DynamicFOV.java
@@ -1,8 +1,10 @@
 package dev.resent.module.impl.misc;
 
+import dev.resent.annotation.Module;
 import dev.resent.module.base.Category;
 import dev.resent.module.base.Mod;
 
+@Module(name = "NoDynamicFOV", category = Category.MISC)
 public class DynamicFOV extends Mod {
 
     public DynamicFOV() {
