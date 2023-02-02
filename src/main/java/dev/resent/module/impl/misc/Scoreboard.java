889ce2b0025b18ae90da09d714c1b95f02e8a76f
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-02 19:56:43 +0000

    port scoreboard

--- src/main/java/dev/resent/module/impl/misc/Scoreboard.java
+++ src/main/java/dev/resent/module/impl/misc/Scoreboard.java
@@ -1,13 +1,14 @@
 package dev.resent.module.impl.misc;
 
+import dev.resent.annotation.Module;
 import dev.resent.module.base.Category;
 import dev.resent.module.base.Mod;
 import dev.resent.module.setting.BooleanSetting;
 
+@Module(name = "Scoreboard", category = Category.MISC, hasSetting = true)
 public class Scoreboard extends Mod {
 
     public Scoreboard() {
-        super("Scoreboard", Category.MISC, true);
         addSetting(numbers);
     }
 
