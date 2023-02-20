09721a6aac17a5c1055013f860189c5191dc4714
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-20 16:00:37 +0000

    Idktbh

--- offline.sh
+++ offline.sh
@@ -2,10 +2,10 @@
 cd javascript
 rm EaglercraftX_1.8_Offline_en_US.html
 rm EaglercraftX_1.8_Offline_International.html
-rm Resent_6.0_patch__1.8_US.html
-rm Resent_6.0_patch_1.8_International.html
+rm Resent_3.6_patch__1.8_US.html
+rm Resent_3.6_patch_1.8_International.html
 cd ../
 ./MakeOfflineDownload.sh
 cd javascript
-mv EaglercraftX_1.8_Offline_en_US.html Resent_6.0_patch__1.8_US.html
-mv EaglercraftX_1.8_Offline_International.html Resent_6.0_patch_1.8_International.html
+mv EaglercraftX_1.8_Offline_en_US.html Resent_3.6_patch__1.8_US.html
+mv EaglercraftX_1.8_Offline_International.html Resent_3.6_patch_1.8_International.html
