63a85f627629f89ad10e9c16bc45d3e7e3e3a71e
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-02-22 01:53:00 +0000

    ui improve

--- src/main/java/dev/resent/module/impl/misc/Cape.java
+++ src/main/java/dev/resent/module/impl/misc/Cape.java
@@ -6,7 +6,7 @@ import dev.resent.module.base.Mod;
 import dev.resent.module.base.Mod.Category;
 import dev.resent.module.setting.CustomRectSettingDraw;
 
-@Module(name = "Cape", category = Category.MISC)
+@Module(name = "Cape", category = Category.MISC, hasSetting = true)
 public class Cape extends Mod{
     public CustomRectSettingDraw open = new CustomRectSettingDraw("Choose cape", "Select which cape you want to use"){
         @Override
