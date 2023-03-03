a36dc95659058bdb285d26fd32a7451f815ea4ec
ThisIsALegitUsername <andrewsouchien@gmail.com>
2023-03-03 19:41:34 +0000

    Offlines

---
+++ soffline.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd javascript/badwifi
+rm EaglercraftX_1.8_Offline_en_US.html
+rm EaglercraftX_1.8_Offline_International.html
+rm Resent_3.6_patch__1.8_US.html
+rm Resent_3.6_patch_1.8_International.html
+cd ../../
+./MakeOfflineDownload.sh
+cd javascript/badwifi
+mv ../EaglercraftX_1.8_Offline_en_US.html ./Resent_3.6_patch__1.8_US.html
+mv ../EaglercraftX_1.8_Offline_International.html ./Resent_3.6_patch_1.8_International.html
