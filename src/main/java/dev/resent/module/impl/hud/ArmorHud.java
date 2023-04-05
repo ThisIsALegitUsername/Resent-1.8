551e608ed19e87844b8a902a6e844237de053596
ThisIsALegitUsername <73719631+ThisIsALegitUsername@users.noreply.github.com>
2023-04-05 08:24:40 -0600

    Update ArmorHud.java

--- src/main/java/dev/resent/module/impl/hud/ArmorHud.java
+++ src/main/java/dev/resent/module/impl/hud/ArmorHud.java
@@ -9,7 +9,7 @@ import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.item.ItemStack;
 
-@RenderModule(name = "ArmorHud", category = Category.HUD, x = 20, y = 4, hasSetting = true)
+@RenderModule(name = "ArmorHud", category = Category.HUD, x = 20, y = 4, hasSetting = true, description = "ArmorHUD")
 public class ArmorHud extends RenderMod {
 
     public ScaledResolution sr;
